b418d0728f91475f38efbebfbd9e88d1a3be4bfd
JONATHAN <jonn.thinkrtc@gmail.com>
2017-10-27 10:26:11 -0700

    created SearchRecipe component

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import SearchRecipe from './SearchRecipe';
 
 
 class App extends Component {
 	render(){
 		return(
 			<div>
-				<h1>RicePad Recipe Finder</h1>
+				<SearchRecipe />
 			</div>
 			)
 	}
--- app/javascript/components/SearchRecipe.jsx
+++ app/javascript/components/SearchRecipe.jsx
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
-
+import {Form, FormControl, FormGroup, ControlLabel, Button } from 'react-bootstrap';
 
 class SearchRecipe extends Component {
 
+	render(){
+		return(
+			<div>
+				<h2>RicePad Recipe Finder</h2>
+				<Form>
+					<FormGroup>
+						<ControlLabel>Find a dish</ControlLabel>
+						<FormControl 
+							type='text' 
+						    placeholder="chicken alfredo, spaghetti"
+						    >
+						</FormControl>
+					</FormGroup>
+					{' '}
+					<FormGroup>
+						<ControlLabel>Find by ingredients</ControlLabel>
+						<FormControl 
+							type='text'  
+							placeholder="salt, pepper, tomatoe">
+						</FormControl>
+					</FormGroup>
+				</Form>
+			</div>
+			)
+	}
 
 }
 
